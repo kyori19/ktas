127945d76a23838bd62330d4d0cc89a4b5b8a707
kyori19 <kyori@accelf.net>
2022-06-11 16:46:37 +0000

    Configure Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.20"
+}
+
+group = "net.accelf"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
